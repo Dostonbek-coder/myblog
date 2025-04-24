ab0976ac9a1d658251d1d5b7f8adee448bdf7147
Dostonbek-coder <mahmudovdostonbek455@gmail.com>
2025-04-24 21:35:55 +0500

    A check has been written to save comment

--- app/Http/Requests/StoreCommentRequest.php
+++ app/Http/Requests/StoreCommentRequest.php
@@ -21,6 +21,7 @@ class StoreCommentRequest extends FormRequest
      */
     public function rules(): array
     {
+        
         return [
             
             "comment"=> "required|max:225",
